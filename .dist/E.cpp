75a43bcc660d45d0e31b7cdbb8d47d08386ef615
ramisa-zahara <ramisazahara@gmail.com>
2023-02-03 00:23:14 +0600

    problem 5

---
+++ .dist/E.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    cout<< fixed << setprecision(9);
+    double r;
+    cin >> r;
+    long double area=3.141592653 * r *r;
+    cout << area <<"\n";
+
+}
