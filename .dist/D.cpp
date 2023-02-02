c712ecbc102ccb05a7886492eccada3052bd894b
ramisa-zahara <ramisazahara@gmail.com>
2023-02-03 00:21:36 +0600

    problem 4

---
+++ .dist/D.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll a,b,c,d;
+    cin >>a >>b >>c >>d;
+    cout<<"Difference = " << (a*b)- (c*d) <<"\n";
+
+}
