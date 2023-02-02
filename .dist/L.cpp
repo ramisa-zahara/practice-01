728f560dc857f528aec666d1850fc558e946670e
ramisa-zahara <ramisazahara@gmail.com>
2023-02-03 00:34:02 +0600

    problem 12

---
+++ .dist/L.cpp
@@ -0,0 +1,9 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    string s1,s2,s3,s4;
+    cin>>s1>>s2>>s3>>s4;
+    (s2==s4) ? cout<< "ARE Brothers"<< "\n" : cout<< "NOT"<< "\n";
+}
