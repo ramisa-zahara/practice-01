88c77c32fae88a75e2f01856b057f9f386412e67
ramisa-zahara <ramisazahara@gmail.com>
2023-02-03 00:19:18 +0600

    problem 3

---
+++ .dist/C.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll a,b;
+    cin >>a >>b;
+    printf("%lld + %lld = %lld\n",a,b,(a+b));
+    printf("%lld * %lld = %lld\n",a,b,(a*b));
+    printf("%lld - %lld = %lld\n",a,b,(a-b));
+}
