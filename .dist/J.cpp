80a001a0c6b158a2d1d94af04fde398eabbd55f1
ramisa-zahara <ramisazahara@gmail.com>
2023-02-03 00:31:04 +0600

    problem 10

---
+++ .dist/J.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    ll a,b;
+    cin>>a>>b;
+    if(a%b==0 || b%a==0)
+        cout<< "Multiples"<< "\n";
+    else
+        cout<< "No Multiples"<< "\n";
+}
