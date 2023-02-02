c4fa0e9776fcaa57d0a1f1d1e7bc50e1eadcf889
ramisa-zahara <ramisazahara@gmail.com>
2023-02-03 00:40:44 +0600

    problem 16

---
+++ .dist/P.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long
+int main()
+{
+    string a;
+    cin>>a;
+    if(a[0]%2 == 0)
+    cout<< "EVEN"<< "\n";
+    else
+        cout<< "ODD"<< "\n";
+
+    return 0;
+}
